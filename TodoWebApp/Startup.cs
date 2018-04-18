7ee9820f37a50e621384aea5a349a56562ea2086
tousif khan <tousif.khan@tal.com.au>
2018-04-18 14:46:00 +1000

    Add project files.

---
+++ TodoWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TodoWebApp.Startup))]
+namespace TodoWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
